d2fae5859f1ea358b883f7c75a4edb6bb437b3cb
abuTalhaOFC <abutalha.officialac@gmail.com>
2022-08-13 15:03:03 +0600

    ofc 2022-08-13

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <MainNav />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainNav from "@/components/MainNav.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainNav,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
