7eb8b39765aa756fc7ac78c19409d23fbe5f6a76
abuTalhaOFC <abutalha.officialac@gmail.com>
2022-08-14 19:57:46 +0600

    ofc

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,19 +4,67 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <!-- Company Logo Start -->
+        <a :href="url" class="flex items-center h-full text-xl">{{
+          company
+        }}</a>
+        <!-- Company Logo end -->
+        <!-- Nav menu start -->
+        <nav class="h-full ml-12">
+          <ul class="flex h-full m-0 p-0 list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="h-full ml-9 first:ml-0"
+            >
+              <a href="" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
+        <!-- Nav menu end -->
+        <!-- SignIn btn start -->
+        <div class="flex items-center h-full ml-auto">
+          <profile-image v-if="isLogedIn" @click="handelClick" />
+          <action-button v-else @click="handelClick" />
+        </div>
+        <!-- SignIn btn end -->
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "./ActionButton.vue";
+import ProfileImage from "./ProfileImage.vue";
+
 export default {
   name: "MainNav",
+
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: "Google Careers",
+      url: "https://react.protalha.com",
+      menuItems: [
+        "Teams",
+        "Location",
+        "Life at PRO",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
+      isLogedIn: false,
     };
+  },
+  methods: {
+    handelClick() {
+      this.isLogedIn ? (this.isLogedIn = false) : (this.isLogedIn = true);
+    },
   },
 };
 </script>
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="imageLink" alt="XX" class="w-8 h-8 object-contain rounded-3xl" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrsKE0n1Wa70pdYeKKIiJonNcU-fuNEV0nuw&usqp=CAU",
+    };
+  },
+};
+</script>
