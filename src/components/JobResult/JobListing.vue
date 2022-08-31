98184c9186ffbacfbae221797be3fe7aff76cfbe
abuTalhaOFC <abutalha.officialac@gmail.com>
2022-08-31 18:27:33 +0600

    ofc

--- src/components/JobResult/JobListing.vue
+++ src/components/JobResult/JobListing.vue
@@ -1,12 +1,12 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/job/result/1"
+      :to="jobPageLink"
       class="block mx-auto pb-5 bg-white border border-solid border-brand-gray-1 rounded hover:shadow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-1">
         <h2 class="mb-2 text-2xl">
-          Software Engineer III, Infrastructure, Google Cloud Networking
+          {{ job.title }}
         </h2>
         <div class="flex flex-row text-sm align-middle">
           <div class="mr-5">
@@ -15,16 +15,25 @@
               size="sm"
               class="mr-2 text-brand-gray-3"
             />
-            <span class="font-semibold">Google</span>
+            <span class="font-semibold">{{ job.organization }}</span>
           </div>
           <div>
-            <span>
+            <span class="flex items-center">
               <font-awesome-icon
                 :icon="['fas', 'location-dot']"
-                size="md"
+                size="sm"
                 class="mr-2 text-brand-gray-3"
               />
-              <span class="font-semibold">In-office: </span>Sunnyvale, CA, USA
+              <span class="font-semibold">In-office: </span>
+              <ul>
+                <li
+                  v-for="location in job.locations"
+                  :key="location"
+                  class="inline-block ml-2"
+                >
+                  {{ location }}
+                </li>
+              </ul>
             </span>
           </div>
         </div>
@@ -35,22 +44,18 @@
           <h3 class="font-semibold">Qualifications:</h3>
         </div>
         <ul class="pl-8 list-disc">
-          <li>Bachelors degree or equivalent practical experience.</li>
-          <li>
-            2 years of experience with software development in one or more
-            programming languages, or 1 year of experience with an advanced
-            degree.
-          </li>
-          <li>
-            2 years of experience with data structures or algorithms in either
-            an academic or industry setting.
+          <li
+            v-for="minimumQualification in job.minimumQualifications"
+            :key="minimumQualification"
+          >
+            {{ minimumQualification }}
           </li>
         </ul>
       </div>
 
       <div class="mt2 text-center">
         <router-link
-          to="/job/result/1"
+          :to="jobPageLink"
           class="text-brand-blue-1 text-sm font-semibold"
           >Expand</router-link
         >
@@ -62,5 +67,16 @@
 <script>
 export default {
   name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/job/result/${this.job.id}`;
+    },
+  },
 };
 </script>
--- src/components/JobResult/JobListings.vue
+++ src/components/JobResult/JobListings.vue
@@ -1,24 +1,69 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <job-listing />
-      <job-listing />
-      <job-listing />
-      <job-listing />
-      <job-listing />
-      <job-listing />
-      <job-listing />
+      <job-listing v-for="job in jobsIncludeTen" :key="job.id" :job="job" />
     </ol>
+    <!-- page number show on left-button -->
+    <div class="mt-8 mx-auto">
+      <div class="flex flex-row flex-nowrap">
+        <p class="text-sm flex-grow">page {{ currentPage }}</p>
+        <!-- Next And Prev Button -->
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            >Previous</router-link
+          >
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JoResults', query: { page: nextPage } }"
+            >Next</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 import JobListing from "@/components/JobResult/JobListing.vue";
 
 export default {
   name: "JobListings",
   components: {
     JobListing,
+  },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  computed: {
+    currentPage() {
+      const pageNumberStr = this.$route.query.page || "1";
+      return Number.parseInt(pageNumberStr);
+    },
+    previousPage() {
+      const prevPage = this.currentPage - 1;
+      const fristPage = 1;
+      return prevPage >= fristPage ? prevPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const lastPage = this.jobs.length / 10;
+      return nextPage <= lastPage ? nextPage : undefined;
+    },
+    jobsIncludeTen() {
+      const startingPage = (this.currentPage - 1) * 10;
+      const endingPage = this.currentPage * 10;
+
+      return this.jobs.slice(startingPage, endingPage);
+    },
+  },
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/jobs");
+    this.jobs = response.data;
   },
 };
 </script>
